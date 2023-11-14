539f23b37790483f66c759c445523df0ffa4275d
Landon Montecalvo <landonmontecalvo290@gmail.com>
2023-11-14 18:40:59 -0500

    Update Flowchart Quiz.py

--- W7D1/Flowchart Quiz.py
+++ W7D1/Flowchart Quiz.py
@@ -1,5 +1,20 @@
+"""
+Variable Dictionary:
+
+bankBalance: inputed starting balance for account.
+initBankBalance: keeps starting value for future use.
+monthlyDeposit: how much money goes into the account every month.
+year: how many years the account will be active for.
+rate: rate that interest increases each year.
+monthCount: current month count.
+month: converts the years into months.
+monthlyInterest: new interest for current month.
+endingBalance: final balance for each month.
+
+"""
+
 bankBalance = float(input("Bank Balance: $"))
-initbankBalance = bankBalance
+initBankBalance = bankBalance
 
 monthlyDeposit = float(input("Monthly Deoposit: $"))
 year = int(input("Year: "))
@@ -26,5 +41,5 @@ while monthCount <= month:
 
 rate *= 100
 
-print("Initial Balance: ${0:.2f}\nMonthly Deopsit: ${1:.2f}\nYear: {2}\nRate: {3}\nEnding Balance: ${4:.2f}".format(initbankBalance, monthlyDeposit, year, rate, endingBalance))
+print("Initial Balance: ${0:.2f}\nMonthly Deopsit: ${1:.2f}\nYear: {2}\nRate: {3}\nEnding Balance: ${4:.2f}".format(initBankBalance, monthlyDeposit, year, rate, endingBalance))
 print("Year: {0}, Final Balance: ${1:.2f}".format(year, endingBalance))
